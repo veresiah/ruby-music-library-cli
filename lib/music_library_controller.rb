f1909a98d70b2bd71b9d3724c14dfc8ca6abd71b
Veresia Hiralall <hiralallveresia@yahoo.com>
2020-12-21 14:24:59 -0500

    commit tree 4937682659a93cf58fd45d622c587ef315e15d2e

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -1,14 +1,12 @@
 class MusicLibraryController
-
-
   def initialize(path = "./db/mp3s")
-    importer = MusicImporter.new(path)
-    importer.import
+    MusicImporter.new(path).import
   end
 
-  def call	  
-    input = ''
-    while input != 'exit'
+  def call
+    input = ""
+
+    while input != "exit"
       puts "Welcome to your music library!"
       puts "To list all of your songs, enter 'list songs'."
       puts "To list all of the artists in your library, enter 'list artists'."
@@ -18,76 +16,73 @@ class MusicLibraryController
       puts "To play a song, enter 'play song'."
       puts "To quit, type 'exit'."
       puts "What would you like to do?"
-      input = gets.chomp
+
+      input = gets.strip
 
       case input
-      when 'list songs'
-        self.list_songs
-      when 'list artists'
-        self.list_artists
-      when 'list genres'
-        self.list_genres
-      when 'list artists'
-        self.list_artists
-      when 'list artist'
-        self.list_songs_by_artist
-      when 'list genre'
-        self.list_songs_by_genre
-      when 'play song'
-        self.play_song
-      else
-        "Type in a valid request please"
+      when "list songs"
+        list_songs
+      when "list artists"
+        list_artists
+      when "list genres"
+        list_genres
+      when "list artist"
+        list_songs_by_artist
+      when "list genre"
+        list_songs_by_genre
+      when "play song"
+        play_song
       end
     end
-  end 
+  end
 
   def list_songs
-    song = Song.all.sort {|a,b| a.name <=> b.name} 
-    song.each.with_index do |s, i|
-      puts "#{i+1}. #{s.artist.name} - #{s.name} - #{s.genre.name}"
-    end
+    Song.all.sort{ |a, b| a.name <=> b.name }.each.with_index(1) do |s, i|
+        puts "#{i}. #{s.artist.name} - #{s.name} - #{s.genre.name}"
+      end
   end
-  
+
   def list_artists
-  artists = Artist.all.sort {|a,b| a.name <=> b.name}
-  artists.each.with_index(1) {|artist, i| 
-  puts "#{i}. #{artist.name}"}
+    Artist.all.sort{ |a, b| a.name <=> b.name }.each.with_index(1) do |a, i|
+      puts "#{i}. #{a.name}"
+    end
   end
 
   def list_genres
-      genres = Genre.all.sort {|a,b| a.name <=> b.name}
-      genres.each.with_index(1) {|genre, i| 
-      puts "#{i}. #{genre.name}"}
+    Genre.all.sort{ |a, b| a.name <=> b.name }.each.with_index(1) do |g, i|
+      puts "#{i}. #{g.name}"
+    end
   end
 
   def list_songs_by_artist
-      puts "Please enter the name of an artist:"
-      input = gets.strip
-  
-      if artist = Artist.find_by_name(input)
-        artist.songs.sort { |a,b| a.name <=> b.name }.each.with_index(1) do |song, i|
-          puts "#{i}. #{song.name} - #{song.genre.name}"
-        end
+    puts "Please enter the name of an artist:"
+    input = gets.strip
+
+    if artist = Artist.find_by_name(input)
+      artist.songs.sort{ |a, b| a.name <=> b.name }.each.with_index(1) do |s, i|
+        puts "#{i}. #{s.name} - #{s.genre.name}"
       end
+    end
   end
 
   def list_songs_by_genre
-      puts "Please enter the name of a genre:"
-      input = gets.strip
+    puts "Please enter the name of a genre:"
+    input = gets.strip
 
-      if genre = Genre.find_by_name(input)
-          genre.songs.sort { |a,b| a.name <=> b.name }.each.with_index(1) do |song, i|
-            puts "#{i}. #{song.artist.name} - #{song.name}"
-          end
+    if genre = Genre.find_by_name(input)
+      genre.songs.sort{ |a, b| a.name <=> b.name }.each.with_index(1) do |s, i|
+        puts "#{i}. #{s.artist.name} - #{s.name}"
       end
+    end
   end
 
   def play_song
-      puts "Which song number would you like to play?"
-      input = gets.strip.to_i
-      if (1..Song.all.length).include?(input)
-        song = Song.all.sort{ |a, b| a.name <=> b.name }[input - 1]
-      end
-      puts "Playing #{song.name} by #{song.artist.name}" if song
+    puts "Which song number would you like to play?"
+
+    input = gets.strip.to_i
+    if (1..Song.all.length).include?(input)
+      song = Song.all.sort{ |a, b| a.name <=> b.name }[input - 1]
+    end
+    puts "Playing #{song.name} by #{song.artist.name}" if song
   end
-end 
+end
--- lib/song.rb
+++ lib/song.rb
@@ -1,85 +1,65 @@
-class Song 
-    attr_accessor :name
-    
-    @@all = []
+class Song
+  attr_accessor :name
+  attr_reader :artist, :genre
 
-    def initialize(name,artist=nil,genre=nil)
-        @name = name
-        self.artist = artist if artist
-        self.genre = genre if genre
-    end
+  @@all = []
 
-    def self.all
-        @@all 
-    end 
+  def initialize(name, artist = nil, genre = nil)
+    @name = name
+    self.artist = artist if artist
+    self.genre = genre if genre
+  end
 
-    def self.destroy_all
-        self.all.clear
-    end
-    
-    def save
-        @@all << self
-    end
-    
-    def self.create(name)
-        self.new(name).tap do|song|
-            song.save
-        end 
-    end
-    
-    def artist=(artist)
-        if @artist == nil
-          @artist = artist
-        else
-          @artist = @artist
-        end
-        if self.artist != nil
-          @artist.add_song(self)
-        end
-        @artist
-      end
+  def artist=(artist)
+    @artist = artist
+    artist.add_song(self)
+  end
 
-      def artist
-        @artist
-      end
-    
-    def genre=(genre)
-        if @genre == nil
-          @genre = genre
-        else
-          @genre= @genre
-        end
-        if self.genre != nil
-          @genre.add_song(self)
-        end
-        @genre
-    end 
-    
-    def genre
-        @genre
-    end
+  def genre=(genre)
+    @genre = genre
+    genre.songs << self unless genre.songs.include?(self)
+  end
 
-    def self.find_by_name(name)
-        @@all.find do |song|
-            song.name == name
-        end
-    end
+  def self.all
+    @@all
+  end
 
-    def self.find_or_create_by_name(name)
-        self.find_by_name(name) || self.create(name)
-    end
+  def self.destroy_all
+    all.clear
+  end
 
-    def self.create_from_filename(name)
-        @@all << self.new_from_filename(name)
-    end
+  def save
+    self.class.all << self
+  end
 
-    def self.new_from_filename(name)
-        song_name = name.split(" - ")[1]
-        artist_name = name.split(" - ")[0]
-        genre_name = name.split(" - ")[2].chomp(".mp3")
-        song = self.find_or_create_by_name(song_name)
-        song.artist = Artist.find_or_create_by_name(artist_name)
-        song.genre = Genre.find_or_create_by_name(genre_name)
-        song
-    end
+  def self.create(name)
+    song = new(name)
+    song.save
+    song
+
+    # Or, as a one-liner:
+    # new(name).tap{ |s| s.save }
+  end
+
+  def self.find_by_name(name)
+    all.detect{ |s| s.name == name }
+  end
+
+  def self.find_or_create_by_name(name)
+    find_by_name(name) || create(name)
+  end
+
+  def self.new_from_filename(filename)
+    parts = filename.split(" - ")
+    artist_name, song_name, genre_name = parts[0], parts[1], parts[2].gsub(".mp3", "")
+
+    artist = Artist.find_or_create_by_name(artist_name)
+    genre = Genre.find_or_create_by_name(genre_name)
+
+    new(song_name, artist, genre)
+  end
+
+  def self.create_from_filename(filename)
+    new_from_filename(filename).tap{ |s| s.save }
+  end
 end
